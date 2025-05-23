0028d7b104ae41fa4c2e9c77072dc9af7405a9f7
urska <urska.vidovic1@student.um.si>
2025-05-23 23:04:22 +0200

    Začetna verzija projektne naloge

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__)
+
+# Pridobi povezavo iz okolja
+mongo_uri = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/mojabaza')
+client = MongoClient(mongo_uri)
+db = client["mojabaza"]  # popravi iz .get_database() v natančno ime baze
+
+@app.route('/')
+def home():
+    db.status.insert_one({"msg": "Pozdrav iz Flask aplikacije!"})
+    return "Pozdravljeni! Aplikacija je povezana z MongoDB."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
